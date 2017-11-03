29dddee6897de3f2ab3f3392791d91c421383306
chriscorrea14 <chris.correa@berkeley.edu>
2017-11-03 11:43:44 -0700

    policy to follow CHOMP trajectories

---
+++ src/dataset.py
@@ -0,0 +1,69 @@
+import pickle
+from os import listdir
+import numpy as np
+
+SDF_DIMENSION = (3,3,4)
+SDF_RESOLUTION = .02
+
+class SDF():
+    def __init__(self):
+        size = [int(dimension / SDF_RESOLUTION) for dimension in SDF_DIMENSION]
+        self.data = np.zeros(size, dtype=np.uint8)
+
+    def add_box(self, position, size):
+        for i in np.arange(-SDF_DIMENSION[0]/2., SDF_DIMENSION[0]/2., SDF_RESOLUTION):
+            if i < position[0] - size[0]/2. or i > position[0] + size[0]/2.:
+                continue
+            for j in np.arange(-SDF_DIMENSION[1]/2., SDF_DIMENSION[1]/2., SDF_RESOLUTION):
+                if j < position[1] - size[1]/2. or j > position[1] + size[1]/2.:
+                    continue
+                for k in np.arange(-SDF_DIMENSION[2]/2., SDF_DIMENSION[2]/2., SDF_RESOLUTION):
+                    if k < position[2] - size[2]/2. or k > position[2] + size[2]/2.:
+                        continue
+                    l = int((SDF_DIMENSION[0]/2. + i)/SDF_RESOLUTION)
+                    m = int((SDF_DIMENSION[1]/2. + j)/SDF_RESOLUTION)
+                    n = int((SDF_DIMENSION[2]/2. + k)/SDF_RESOLUTION)
+                    self.data[l,m,n] = 1
+
+def generate_sdf(i):
+    file = str(i) + "_box_position.pkl"
+    sdf = SDF()
+    position = pickle.load(open("../trajectories/" + file, "rb"))
+    sdf.add_box(position, (.5, .7, .1))
+    return sdf.data
+
+def generate_state_action(i):
+    file = str(i) + "_plan.pkl"
+    trajectory = pickle.load(open("../trajectories/" + file, "rb"))
+    trajectory = trajectory.joint_trajectory.points
+    states, actions = [], []
+    for j in range(len(trajectory) - 1):
+        action = np.subtract(trajectory[j+1].positions, trajectory[j].positions)
+        # action = action / (100 * np.linalg.norm(action))
+        states.append(trajectory[j].positions)
+        actions.append(action)
+    states = np.array(states)
+    actions = np.array(actions)
+    return states, actions
+
+
+def generate_dataset():
+    files = listdir("../trajectories/")
+    sdfs, states, actions, sdf_indices = [], [], [], []
+    for i in range(len(files)/2):
+        sdfs.append(generate_sdf(i))
+        state, action = generate_state_action(i)
+        sdf_indices.append([i-1] * state.shape[0])
+        states.append(state)
+        actions.append(action)
+
+    sdf_indices = np.hstack(sdf_indices)
+    sdfs = np.array(sdfs)
+    states = np.vstack(states)
+    actions = np.vstack(actions)
+
+    return sdfs, sdf_indices, states, actions
+
+if __name__ == "__main__":
+    generate_dataset()
+    # generate_sdf()
---
+++ src/main.py
@@ -0,0 +1,131 @@
+import tensorflow as tf
+import numpy as np
+from random import shuffle
+from dataset import generate_dataset, SDF
+from replanning_demo import RobotController
+import pickle
+import rospy
+from geometry_msgs.msg import PoseStamped
+
+SDF_DIMENSION = (3,3,4)
+SDF_RESOLUTION = .02
+# 6 for Fanuc, 7 for YuMi
+ARM_DIMENSION = 6
+LEARNING_RATE = 5e-4
+ITERATIONS = 40
+BATCH_SIZE = 50
+
+def get_model(sdf, state, num_actions, scope, reuse=False):
+    with tf.variable_scope(scope, reuse=reuse):
+        sdf_out = tf.layers.conv2d(sdf,     filters=32, kernel_size=8, strides=4, activation=tf.nn.relu)
+        sdf_out = tf.layers.conv2d(sdf_out, filters=64, kernel_size=4, strides=2, activation=tf.nn.relu)
+        sdf_out = tf.layers.conv2d(sdf_out, filters=64, kernel_size=3, strides=1, activation=tf.nn.relu)
+        
+        flattened = tf.contrib.layers.flatten(sdf_out)
+        # print flattened.get_shape()
+        # print state.get_shape()
+        
+        # dim = np.prod(tf.shape(sdf_out)[1:])
+        # flattened = tf.reshape(sdf_out, [-1, dim])
+        out = tf.concat((flattened, state), axis=1)
+
+        out = tf.layers.dense(out, 256,         activation=tf.nn.relu)
+        out = tf.layers.dense(out, 256,         activation=tf.nn.relu)
+        out = tf.layers.dense(out, num_actions, activation=None)
+    return out
+
+def learn():
+    session = tf.Session()
+
+    size = [dimension / SDF_RESOLUTION for dimension in SDF_DIMENSION]
+    sdf_ph = tf.placeholder(tf.uint8, [None] + size)
+    sdf_ph_float = tf.cast(sdf_ph, tf.float32)
+    state_ph = tf.placeholder(tf.float32, [None, ARM_DIMENSION])
+
+    predicted_action = get_model(sdf_ph_float, state_ph, ARM_DIMENSION, "policy", reuse=False)
+    action = tf.placeholder(tf.float32, [None, ARM_DIMENSION])
+
+    loss = tf.reduce_mean(tf.square(predicted_action - action))
+    update_op = tf.train.AdamOptimizer(LEARNING_RATE).minimize(loss)
+
+    sdfs, sdf_indices, states, actions = generate_dataset()
+    indices = np.arange(states.shape[0])
+
+    session.__enter__()
+    tf.global_variables_initializer().run()
+
+    for i in range(ITERATIONS):
+        shuffle(indices)
+        batch = indices[:BATCH_SIZE]
+        sdf_batch = sdfs[sdf_indices[batch]]
+
+        if i % 10 == 0:
+            current_loss = session.run(loss, feed_dict={
+                sdf_ph: sdf_batch, 
+                state_ph: states[batch], 
+                action: actions[batch]
+            })
+
+            print "Loss at iteration", i, ": ", current_loss
+
+        session.run(update_op, feed_dict={
+                sdf_ph: sdf_batch, 
+                state_ph: states[batch], 
+                action: actions[batch]
+            })
+
+    # evaluation
+    position = np.random.normal(scale=0.1, size=3) + np.array([1,0,1])
+    print "box position:", position
+    sdf = SDF()
+
+
+
+    states = []
+    state = np.array([0.0] * 6)
+    for _ in range(100):
+        a = session.run(predicted_action, feed_dict={sdf_ph: [sdf.data], state_ph: [state]})[0]
+        state = state + a
+        states.append(state)
+
+    # sdf.add_box(position, (.5, .7, .1))
+    # for _ in range(95):
+    #     a = session.run(predicted_action, feed_dict={sdf_ph: [sdf.data], state_ph: [state]})[0]
+    #     state = state + a
+    #     states.append(state)
+
+
+
+    states = np.array(states)
+    print states
+    file = "tmp.pkl"
+    with open(file, 'wb') as output:
+        pickle.dump(states, output, pickle.HIGHEST_PROTOCOL)
+
+def display_trajectory():
+    file = "tmp.pkl"
+    trajectory = pickle.load(open(file, "rb"))
+    rospy.init_node('robot_controller')
+    robot_controller = RobotController()
+
+    pose = PoseStamped()
+    # pose.header.frame_id = robot.get_planning_frame()
+    box_position = [0.80702869, 0.05520982, 0.93043837]
+    pose.pose.position.x = box_position[0]
+    pose.pose.position.y = box_position[1]
+    pose.pose.position.z = box_position[2]
+    pose.pose.orientation.w = 1
+    pose.pose.orientation.x = 0
+    pose.pose.orientation.y = 0
+    pose.pose.orientation.z = 0
+    robot_controller.scene.add_box("aaa", pose, size=(0.5, .7, 0.1))
+    rospy.sleep(2)
+
+    for point in trajectory:
+        robot_controller.publish_joints(point)
+        rospy.sleep(.1)
+
+if __name__ == "__main__":
+    # learn()
+    # evaluate()
+    display_trajectory()
